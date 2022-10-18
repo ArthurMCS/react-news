ea2515e4905a3cdf122b77a50bf95cf4ddd83f8e
Arthur Moreira <arthurmcs96@gmail.com>
2022-10-18 00:35:40 -0300

    começa a construir a pagina dos posts

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,4 +1,9 @@
+/* eslint-disable react/no-danger */
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import { RichText } from 'prismic-dom';
+import { ReactElement } from 'react';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
@@ -11,6 +16,10 @@ interface Post {
     title: string;
     banner: {
       url: string;
+      dimensions: {
+        width: number;
+        height: number;
+      };
     };
     author: string;
     content: {
@@ -26,20 +35,53 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps): ReactElement {
+  return (
+    <>
+      <Header />
+      <main className={styles.postContainer}>
+        <img src={post.data.banner.url} alt="banner" />
+        <section>
+          <h1>{post.data.title}</h1>
+          <div className={styles.postContent}>
+            {post.data.content.map(content => (
+              <>
+                <h2>{content.heading}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(content.body),
+                  }}
+                />
+              </>
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
+export const getStaticPaths = async (): Promise<any> => {
+  const prismic = getPrismicClient({});
+  const posts = await prismic.getByType('posts', {
+    lang: 'pt-BR',
+    pageSize: 5,
+  });
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps = async ({ params }): Promise<any> => {
+  const { slug } = params;
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('posts', String(slug), {});
 
-//   // TODO
-// };
+  return {
+    props: {
+      post: response,
+    },
+  };
+};
